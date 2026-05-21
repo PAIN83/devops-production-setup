4488a7e57e474d9cdb6da95fc308d2ccb08a5a69
Pain <ashwantnandan118@gmail.com>
2026-05-21 08:17:11 +0000

    Update to version 3 in app.py

--- 02-cicd-pipeline/app.py
+++ 02-cicd-pipeline/app.py
@@ -7,7 +7,7 @@ def home():
     return """
     <h1>DevOps Project 2</h1>
     <p>Deployed automatically via GitHub Actions CI/CD Pipeline</p>
-    <p>Version 2 — Pipeline is working!</p>
+    <p>Version 3 — Pipeline is working!</p>
     """
 
 @app.route("/health")
